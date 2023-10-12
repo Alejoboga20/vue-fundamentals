73afd864bd40e73761663475b44feb7e1c83ca02
Alejo Boga <alejoboga20@gmail.com>
2023-10-12 10:47:45 -0300

    verify html content

--- src/components/Counter.spec.ts
+++ src/components/Counter.spec.ts
@@ -2,9 +2,21 @@ import { shallowMount } from '@vue/test-utils';
 import Counter from './Counter.vue';
 
 describe('Counter.vue', () => {
-	test('should match the snapshot', () => {
+	const DEFAULT_TITLE = 'Counter';
+
+	test.skip('should match the snapshot', () => {
 		const wrapper = shallowMount(Counter);
 
 		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	test('should have the default title', () => {
+		const wrapper = shallowMount(Counter);
+
+		const titleElement = wrapper.find('h2').exists();
+		expect(titleElement).toBeTruthy();
+
+		const title = wrapper.find('h2').text();
+		expect(title).toBe(DEFAULT_TITLE);
 	});
 });
